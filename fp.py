3e2dd63d673fd864048a78d64d91b5a79c39e7c9
huhuhu0420 <ghostcheng.00@gmail.com>
2023-11-03 19:15:21 +0800

    mlx

--- fp.py
+++ fp.py
@@ -1,10 +1,10 @@
 from fpgrowth_py import fpgrowth
 from get_author import get_author
 
-data = get_author()
+data = get_author('./dataset/author_10.txt')
 print(len(data))
 print('get data')
 
-freqItemSet, rules = fpgrowth(data, minSupRatio=0.000001, minConf=0.000001)
-print(freqItemSet)
+freqItemSet, rules = fpgrowth(data, minSupRatio=0.2, minConf=0.1)
+# print(freqItemSet)
 print(rules)  
