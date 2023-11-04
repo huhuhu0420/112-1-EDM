3c29c15f7bc4229af4d3b8fd0bf0cbe9870d807a
huhuhu0420 <ghostcheng.00@gmail.com>
2023-11-04 16:18:34 +0800

    update

--- divide_data.py
+++ divide_data.py
@@ -3,17 +3,17 @@ from get_author import get_author
 
 def test():
     datas = get_author('./dataset/author.txt')
+    print("get data")
     # datas = [['a', 'b', 'c'], ['e', 'f', 'g'], ['a', 'b', 'c', 'd'], ['a', 'b', 'c', 'd'], ['a', 'b', 'c', 'd', 'z'], ['g', 'e']]
     divides = []
     flag = 0
     for data in datas:
         dataset  = set(data)
-        for divide in divides:
-            flag = 0
+        flag = 0
+        for divide in divides:  
             if dataset.isdisjoint(divide[0]) == False:
                 # print(dataset, divide[0])
                 flag = 1
-            if flag == 1:
                 divide.append(dataset)
                 break
         if flag == 0:
@@ -21,8 +21,8 @@ def test():
     
     i = 0
     for divide in divides:
-        path = './divide_data/' + str(i) + '.txt'
-        print(divide)
+        path = './divide_data_v2/' + str(i) + '.txt'
+        # print(divide)
         with open(path, 'w') as f:
             for d in divide:
                 f.writelines(','.join(d) + '\n')
